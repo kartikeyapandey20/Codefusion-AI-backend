cfc2fd9d6c3475ffcfcb4045ac76e3559e347248
Kartikeya Pandey <kartikeyapandey20@gmail.com>
2025-04-23 15:20:02 +0530

    did some changes in conver_system_message_to_human

--- api/v1/chat/repository.py
+++ api/v1/chat/repository.py
@@ -14,6 +14,7 @@ class ChatRepository:
         self.llm = ChatGoogleGenerativeAI(
             model="gemini-2.0-flash",
             temperature=0.7,
+            convert_system_message_to_human=True  
         )
 
     async def create_chat_session(self, user_id: int,session_title:str) -> int:
